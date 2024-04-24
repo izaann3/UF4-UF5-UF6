584fa966bff17af0815e924dd64e13644dc63ff2
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-04-24 19:56:34 +0200

    error al compilar el codigo creado por primera vez
    codificacióo_classes.java:18: error: invalid method declaration; return type required
        public setNom(String n) {
               ^
    codificacióo_classes.java:22: error: invalid method declaration; return type required
        public setAutor(String a) {
               ^
    codificacióo_classes.java:26: error: invalid method declaration; return type required
        public setDurada(int d) { 
               ^
    3 errors

---
+++ codificacio_classes.java
@@ -0,0 +1,49 @@
+class Media {
+    private String nom;
+    private String autor;
+    private int duradaSegons;
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public int getDurada() {
+        return this.duradaSegons;
+    }
+
+    public setNom(String n) {
+        this.nom = n;
+    }
+
+    public setAutor(String a) {
+        this.autor = a;
+    }
+
+    public setDurada(int d) { 
+        this.duradaSegons = d;
+    }
+}
+
+class Main {
+    public static final void main(String[] args) {
+        Media poner_nombre = new Media();
+        Media poner_autor = new Media();
+        Media poner_durada = new Media();
+
+        poner_nombre.setNom("izan");
+        poner_autor.setAutor("Jr.Albert");
+        poner_durada.setDurada(12);
+
+        String nombre_correcte = poner_nombre.getNom();
+        String autor_correcte = poner_autor.getAutor();
+        int durada_correcte = poner_durada.getDurada();
+        
+        System.out.println(nombre_correcte);
+        System.out.println(autor_correcte);
+        System.out.println(durada_correcte);
+    }
+}
